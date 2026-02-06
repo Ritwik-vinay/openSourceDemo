f5f242af99b6262bffe491019db9b13c244767e6
ritwik vinay <ritwikvinay38@gmail.com>
2026-02-07 00:39:27 +0530

    Initial Commit -Updated on Feb26

--- src/main/java/driver/DriverManager.java
+++ src/main/java/driver/DriverManager.java
@@ -11,7 +11,7 @@ public class DriverManager {
     public static void setDriver(WebDriver webDriver){
         driver.set(webDriver);
     }
-    public static WebDriver getDriver(){
+    protected WebDriver getDriver(){
         return driver.get();
     }
     public static void unload(){
